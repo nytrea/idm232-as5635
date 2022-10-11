042f71eb375a05dab9efaf3b2a41e5422e5cd28c
nytrea <anoushsan@gmail.com>
2022-10-11 16:09:00 -0400

    first commit

---
+++ final/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+
+<body>
+    <h1> IDM 232 Final </h1>
+    <script> src="dist/scripts/main.js"></script>
+
+</body>
+</html>
+
+<!-- make a folder called components in final folder and make a footer.php file and a header.php file -->
+
+in header.php:
+
+<?php>  include 'components.header.php'; ?>
+    <h1> My IDM 232 Final </h1>
+<?php> include 'components/footer.php'; ?>
+
